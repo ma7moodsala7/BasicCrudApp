808a658da2b8ebd84e8032225cb800f257972cbb
Mahmoud Salah <ma7mood.sala7@gmail.com>
2021-10-07 08:57:53 +0200

    Adding Fluent Validation Nuget Package

---
+++ LegalAdvice.Application/Features/Request/Commands/CreateRequest/CreateRequestCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace LegalAdvice.Application.Features.Request.Commands.CreateRequest
+{
+    public class CreateRequestCommandValidator : AbstractValidator<CreateRequestCommand>
+    {
+        public CreateRequestCommandValidator()
+        {
+            RuleFor(p => p.Description)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .NotNull();
+        }
+    }
+}
